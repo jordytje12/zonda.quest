7247a90777dada673cf038686b20e26066d80299
jordytje12 <jordy.vzanten2000@gmail.com>
2024-11-03 22:01:13 +0100

    new migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Jordy van Zanten</title>
-    @vite('resources/css/app.css')
 </head>
 <body>
 <h1 class="font-bold text-2xl">Hello world</h1>
-@vite('resources/js/app.js')
 </body>
 </html>
