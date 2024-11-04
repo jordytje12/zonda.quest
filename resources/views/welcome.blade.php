797e472fa32c1131b3f79382c7a002c9cf1882af
jordytje12 <jordy.vzanten2000@gmail.com>
2024-11-04 22:23:54 +0100

    new migration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,8 @@
     <header class="absolute inset-x-0 top-0 z-50">
         <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div class="flex lg:flex-1">
-                <a href="#" class="-m-1.5 p-1.5">
-                    <span class="sr-only">Your Company</span>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt="">
+                <a href="/" class="-m-1.5 p-1.5">
+                    <img class="h-8 w-auto" src="/resources/images/jorddev%20discord.jpg" alt="">
                 </a>
             </div>
             <div class="flex lg:hidden">
@@ -20,13 +19,12 @@
                 </button>
             </div>
             <div class="hidden lg:flex lg:gap-x-12">
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Product</a>
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Features</a>
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Marketplace</a>
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Company</a>
+                <a href="#" class="text-sm/6 font-semibold text-gray-900">Over mij</a>
+                <a href="#" class="text-sm/6 font-semibold text-gray-900">Portfolio</a>
+                <a href="#" class="text-sm/6 font-semibold text-gray-900">Blog</a>
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+                <a href="#" class="text-sm/6 font-semibold text-gray-900">Contact <span aria-hidden="true">&rarr;</span></a>
             </div>
         </nav>
         <!-- Mobile menu, show/hide based on menu open state. -->
